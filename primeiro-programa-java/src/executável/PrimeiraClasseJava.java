e2b77a40cfa439dcd868e1d57caa47120492cc91
Angela & Felipe <Angela & Felipe@AngelaFelipe>
2021-10-06 13:00:35 -0300

    SWDQWD

---
+++ primeiro-programa-java/src/executável/PrimeiraClasseJava.java
@@ -0,0 +1,25 @@
+package executável;
+
+import cursojava.Aluno;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Aluno aluno1 = new Aluno(null);
+		
+		Aluno aluno2 = new Aluno(null);
+		
+		Aluno aluno3 = new Aluno(null);
+		
+		Aluno aluno4 = new Aluno(null);
+		
+		Aluno aluno5 = new Aluno("Maria");
+		
+		Aluno aluno6 = new Aluno("Joao", 50);
+		
+		
+	}
+
+}
