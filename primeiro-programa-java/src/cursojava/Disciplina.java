4882f92f01a9f586c1749e841c6411b5ac16fc56
Angela & Felipe <Angela & Felipe@AngelaFelipe>
2021-10-09 12:05:05 -0300

    adw

---
+++ primeiro-programa-java/src/cursojava/Disciplina.java
@@ -0,0 +1,63 @@
+package cursojava;
+
+public class Disciplina {
+	
+	 private double nota1;
+	 private double nota2;
+	 private double nota3;
+	 private double nota4;
+	
+	 public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public double getNota3() {
+		return nota3;
+	}
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+	public double getNota4() {
+		return nota4;
+	}
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+	public String getDisciplina1() {
+		return Disciplina1;
+	}
+	public void setDisciplina1(String disciplina1) {
+		Disciplina1 = disciplina1;
+	}
+	public String getDisciplina2() {
+		return Disciplina2;
+	}
+	public void setDisciplina2(String disciplina2) {
+		Disciplina2 = disciplina2;
+	}
+	public String getDisciplina3() {
+		return Disciplina3;
+	}
+	public void setDisciplina3(String disciplina3) {
+		Disciplina3 = disciplina3;
+	}
+	public String getDisciplina4() {
+		return Disciplina4;
+	}
+	public void setDisciplina4(String disciplina4) {
+		Disciplina4 = disciplina4;
+	}
+	private String Disciplina1;
+	 private String Disciplina2;
+	 private String Disciplina3;
+	 private String Disciplina4;
+
+}
